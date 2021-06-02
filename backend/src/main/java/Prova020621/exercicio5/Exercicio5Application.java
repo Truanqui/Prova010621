61ec0d9b1fcdff0687659099036e6e46d2f79625
[johann] <johannteixeira2@gmail.com>
2021-06-02 01:44:02 -0300

    Primeira versão do projeto

---
+++ backend/src/main/java/Prova020621/exercicio5/Exercicio5Application.java
@@ -0,0 +1,13 @@
+package Prova020621.exercicio5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio5Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Prova020621/exercicio5/Exercicio5ApplicationTests.java
@@ -0,0 +1,13 @@
+package Prova020621.exercicio5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
